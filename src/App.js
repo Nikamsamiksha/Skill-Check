06919ba28688a32bf61bede85bd344134756bc5b
Vanshika Raheja <vanshikaraheja27@gmail.com>
2024-08-20 19:38:47 +0530

    refactor connected backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import Login from './components/Login'
+import { AuthProvider } from "../src/contexts/AuthContext";
 
 export default function App() {
   return (
-    <div>App</div>
+    <AuthProvider>
+        <Login />
+    </AuthProvider>
   )
 }
